1b7b141e47710b14221b2634abdf58a794705ae9
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2026-07-05 00:42:16 +1200

    Fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'WEEZOOKA'
+description 'A simple script to handle discord profile widgets'
+version '1.0.0'
+lua54 'yes'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua',
+} 
